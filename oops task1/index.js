54d74f40dcd6bf0d925da3932b2226cdeee1b7dd
Nisha Tailor <tailornisha26@gmail.com>
2024-08-01 17:00:49 +0530

    Javascript

--- oops task1/index.js
+++ oops task1/index.js
@@ -1,18 +1,19 @@
-class employee{
-    constructor(name,number,job,salary){
-        this.name=name
-        this.number=number
-        this.job=job
-        this.salary=salary
+class employee {
+    constructor(name, number, job, salary) {
+        this.name = name
+        this.number = number
+        this.job = job
+        this.salary = 0
     }
-    leave(name){
-        console.log("you can take leave");
+    leave() {
+        if (employee)
+            console.log("you can take leave");
     }
-    resign(){
-        console.log("hello");
+    resign() {
+        console.log("Goodbye!!!");
     }
 }
-let emp=new employee("Nisha",9687590929,"BackEnd developer",15000)
+let emp = new employee("Nisha", 9687590929, "BackEnd developer", 15000)
 console.log(emp);
-emp.leave()
+emp.leave(10000)
 emp.resign()
