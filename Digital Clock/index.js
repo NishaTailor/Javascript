a5294778e5c96a8286e5b88164a0ee78927c9c4e
Nisha Tailor <tailornisha26@gmail.com>
2024-08-16 16:44:21 +0530

    Javascript

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -1,11 +1,18 @@
 const date = new Date()
 setInterval(() => {
-    const date=new Date()
+    const date = new Date()
     // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-    document.getElementById("hrs").innerHTML=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
-}, 1000);
+    document.getElementById("hrs").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+}, 1000); 
 
 let Day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+setTimeout(() => {
+    const date = new Date()
+    document.getElementById("day").innerHTML =Day[5] + ", " + date.getDate() + "  " + months[7]
+}, 86,400);
+
+
 
 
