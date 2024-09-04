3d9b914bfd33ed0cf754304fa850df74a986e2c2
Nisha Tailor <tailornisha26@gmail.com>
2024-09-04 17:15:52 +0530

    Project

--- Project/components/helper.js
+++ Project/components/helper.js
@@ -1,4 +1,18 @@
 const getValue = (className) => {
-    return document.querySelector(className).value
-}
-export default getValue
+    return document.querySelector(className).value;
+};
+
+export default getValue;
+
+export const createTag = (tag, value) => {
+    let tagName = document.createElement(tag);
+    if (tag == "img") {
+        tagName.src = value;
+    }
+    else {
+        tagName.innerHTML = value;
+    }
+
+
+    return tagName;
+};
---
+++ Project/js/product.js
@@ -0,0 +1,24 @@
+import { createTag } from "../components/helper.js";
+import Navbar from "../components/navbar.js";
+
+document.getElementById("navbar").innerHTML = Navbar();
+
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+
+
+const mapper = (data) => {
+
+    data.map((ele) => {
+
+        let img = createTag("img", ele.img)
+        let price = createTag("h4", ele.price)
+        let title = createTag("h2", ele.title)
+        let category = createTag("p", ele.category)
+        let div = document.createElement("div")
+        div.append(img, title, price, category)
+        document.getElementById("productList").append(div)
+    })
+
+}
+mapper(products)
