72313f4f1528e22d66b1f782e242961af9c8d15a
Nisha Tailor <tailornisha26@gmail.com>
2024-10-03 15:34:58 +0530

    JsfinalExam-2

---
+++ JsfinalExam-2/js/add.js
@@ -0,0 +1,27 @@
+import getValue from "../components/helper.js";
+let isLogin = localStorage.getItem("isLogin") || false;
+
+if (isLogin == false) {
+    window.location.href = "/pages/login.html";
+}
+
+
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let product = {
+        title: getValue(".title"),
+        price: getValue(".price"),
+        category: getValue(".category"),
+        img: getValue(".img"),
+        id: Date.now()
+    };
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+
+};
+
+document.getElementById("productData").addEventListener("submit", handleSubmit);
